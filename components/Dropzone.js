b9838dc8c0397f973d1b7c582e0a65f6a3e7eebc
EnriqueA75 <48929949+EnriqueA75@users.noreply.github.com>
2021-11-14 16:52:33 -0600

    files accepted list and conditional upload

--- components/Dropzone.js
+++ components/Dropzone.js
@@ -3,11 +3,43 @@ import { useDropzone } from "react-dropzone";
 import clienteAxios from '../config/axios'
 
 const Dropzone = () => {
+    
+    const onDrop = useCallback( async (acceptedFiles) => {
+        console.log(acceptedFiles)
+        const formData = new FormData()
+        formData.append('Archivo', acceptedFiles[0])
+        const resultado = await clienteAxios.post('/api/archivos', formData)
+        console.log(resultado.data)
+    })
+    const crearEnlace = () => {
+        console.log('creando el enlace')
+    }
 
     //extraer contenido del dropzone
-    const { getRootProps, getInputProps, isDragActive } = useDropzone()
+    const { getRootProps, getInputProps, isDragActive, acceptedFiles } = useDropzone({onDrop})
+
+    const archivos = acceptedFiles.map(archivo => (
+        <li key={archivo.lastModified} className="bg-white flex-1 p-3 mb-4 shadow-lg rounded">
+            <p className="font-bold text-xl">{archivo.path}</p>
+            <p className="text-sm text-gra-500">{((archivo.size) / Math.pow(1024, 2)).toFixed(2)} MB bytes</p>
+        </li>
+    ))
+
     return ( 
         <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100">
+            {acceptedFiles.length > 0 ? (
+            <div className="mt-10 w-full">
+                <h4 className="text-2xl font-bold text-center mb-4">Archivos</h4>
+                <ul>
+                    {archivos}
+                </ul>
+                <button className="bg-blue-600 w-full py-3 rounded-lg text-white my-10  hover:bg-blue-800" type="button"
+                        onClick={() => crearEnlace()}
+                >
+                    Crear Enlace
+                </button>
+            </div>
+            ) : (
             <div {...getRootProps({className: 'dropzone w-full py-32'})}>
                 <input className="h-100" {...getInputProps()}/>
                     {
@@ -18,6 +50,7 @@ const Dropzone = () => {
                         </div>
                     }
             </div>
+            )} 
         </div>
      );
 }
